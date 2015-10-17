29161a4653107a9c685c9a79274c23198568bfd3
Mario Gabriell <mariogk01@gmail.com>
2015-10-17 18:12:31 -0300

    Ahri

--- AiBuddy/AiBuddy/Champions/Ahri/Ahri.cs
+++ AiBuddy/AiBuddy/Champions/Ahri/Ahri.cs
@@ -1,6 +1,61 @@
-﻿namespace AiBuddy.Champions.Ahri
+﻿#region
+
+using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+
+#endregion
+
+namespace AiBuddy.Champions.Ahri
 {
+    // MarioGK
     internal class Ahri
     {
+        public static Spell.Skillshot Q;
+        public static Spell.Active W;
+        public static Spell.Skillshot E;
+        public static Spell.Active R;
+
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static void Initialize()
+        {
+            Bootstrap.Init(null);
+
+            InitSpells();
+            InitMisc();
+        }
+
+        public static void InitSpells()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 880, SkillShotType.Linear, 350, 1700, 70);
+            W = new Spell.Active(SpellSlot.W, 550);
+            E = new Spell.Skillshot(SpellSlot.E, 975, SkillShotType.Linear, 350, 1600 , 55);
+            R = new Spell.Active(SpellSlot.R, 450);
+        }
+
+        private static void InitMisc()
+        {
+            Game.OnUpdate += OnGameUpdate;
+            Gapcloser.OnGapcloser += Gapcloser_OnGapcloser;
+        }
+
+        private static void Gapcloser_OnGapcloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
+        {
+            if (sender.IsEnemy && sender.IsValidTarget(Q.Range) && sender != null && e != null)
+            {
+                E.Cast(sender);
+            }
+        }
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+            Modes.Combo.Execute();
+        }
     }
 }
--- AiBuddy/AiBuddy/Champions/Ahri/Modes/Combo.cs
+++ AiBuddy/AiBuddy/Champions/Ahri/Modes/Combo.cs
@@ -1,6 +1,47 @@
-﻿namespace AiBuddy.Champions.Ahri.Modes
+﻿#region
+
+using AiBuddy.AI.Maps.HowlingAbyss.Brain;
+using AiBuddy.Utils;
+using EloBuddy;
+using EloBuddy.SDK;
+
+#endregion
+
+namespace AiBuddy.Champions.Ahri.Modes
 {
     internal class Combo
     {
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static void Execute()
+        {
+
+            var target = GetTarget.GetComboTarget();
+            if (target == null) return;
+
+            if (Ahri.E.IsReady() && target.IsValidTarget(Ahri.E.Range))
+            {
+                Ahri.E.Cast(target);
+            }
+
+            if (Ahri.Q.IsReady() && target.IsValidTarget(Ahri.Q.Range))
+            {
+                Ahri.Q.Cast(target);
+            }
+
+            if (Ahri.W.IsReady() && target.IsValidTarget(Ahri.W.Range))
+            {
+                Ahri.W.Cast(target);
+            }
+
+            if (Ahri.R.IsReady() && target.IsValidTarget(Ahri.R.Range) &&
+                _Player.CountEnemiesInRange(Ahri.Q.Range) <= 2 && target.HealthPercent <= 30 && ComboWinPrediction.Calculate() <= 70)
+            {
+                Ahri.R.Cast(target);
+            }
+        }
     }
 }
