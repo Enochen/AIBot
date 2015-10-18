afd5919d185788541292f8f82f6dd73627b8669c
Darakath <darakath1128@gmail.com>
2015-10-18 18:51:32 -0400

    Not sure what im doing :confused:

---
+++ AiBuddy/AiBuddy/AI/Maps/HowlingAbyss/Brain/DarakathStuff.cs
@@ -0,0 +1,23 @@
+﻿namespace AiBuddy.AI.Maps.HowlingAbyss.Brain
+{
+    using System.Linq;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+
+    using SharpDX;
+
+    internal class DarakathStuff
+    {
+        public static Vector3 HealPosition()
+        {
+            var heal =
+                ObjectManager.Get<GameObject>()
+                    .Where(o => o.Name.Contains("healingBuff"))
+                    .OrderBy(buff => buff.Distance(Player.Instance))
+                    .FirstOrDefault();
+
+            return heal?.Position ?? new Vector3(1,2,3);
+        }
+    }
+}
