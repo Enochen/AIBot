63b61ee8e3f37aa0ecbd077555758f56d8a38f35
Darakath <darakath1128@gmail.com>
2015-10-18 20:31:51 -0400

    test?

--- AiBuddy/AiBuddy/Init.cs
+++ AiBuddy/AiBuddy/Init.cs
@@ -7,23 +7,67 @@ using EloBuddy.SDK.Events;
 
 namespace AiBuddy
 {
+    using System.Linq;
+    using System.Reflection;
+
+    using AiBuddy.AI.Automation;
+
+    using EloBuddy;
+
+    using TreeSharp;
+
     internal class Init
     {
-        public static Utils.AIWalker AIWalker;
+        //public static Utils.AIWalker AIWalker;
 
-        private static void Main()
+        //private static void Main()
+        //{
+        //    // Once loading is complete we will now call Initialize()
+        //    Loading.OnLoadingComplete += Initialize;
+        //}
+
+        //private static void Initialize(EventArgs args)
+        //{
+        //    // Champion Init
+        //    //Switches between your current champion
+
+        //    AIWalker = new Utils.AIWalker();
+        //    AI.InitBrain.Load();
+        //}
+
+        private static GameRoutine Routine;
+
+        private static void Main(string[] args)
         {
-            // Once loading is complete we will now call Initialize()
-            Loading.OnLoadingComplete += Initialize;
-        }
+            Loading.OnLoadingComplete += delegate
+            {
+                foreach (var routine in
+                    Assembly.GetExecutingAssembly()
+                        .GetTypes()
+                        .Where(p => typeof(GameRoutine).IsAssignableFrom(p) && !p.IsAbstract)
+                        .Select(Activator.CreateInstance)
+                        .OfType<GameRoutine>()
+                        .Where(routine => Game.MapId == routine.MapId))
+                {
+                    Routine = routine;
 
-        private static void Initialize(EventArgs args)
-        {
-            // Champion Init
-            //Switches between your current champion
+                    Routine.OnLoad();
+                }
 
-            AIWalker = new Utils.AIWalker();
-            AI.InitBrain.Load();
+                Game.OnTick += delegate
+                {
+                    Routine.PrimaryBehaviour.Tick(null);
+
+                    if (Routine.PrimaryBehaviour.LastStatus == RunStatus.Running)
+                    {
+                        return;
+                    }
+                    Routine.PrimaryBehaviour.Stop(null);
+                    Routine.PrimaryBehaviour.Start(null);
+                };
+
+                Drawing.OnDraw += delegate { Routine.OnDraw(); };
+            };
         }
     }
 }
